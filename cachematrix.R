86006f3539c8b67be7fd36e8f466e917d63f2750
HDking <h.c.dinh1@gmail.com>
2015-10-20 17:04:20 +0200

    first adjustments to the file

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions. A short description on their functionalities:
+## 1) makeCacheMatrix: Create a special "matrix" object that can cache its inverse
+## 2) cacheSolve: Computes the inverse of the special "matrix" returned by makeCacheMatrix. If the inverse has already been calculated, the the cachesolve should retrieve the inverse from the cache 
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #Set the value of the matrix
+  #Get the value of the vector
+  #Set the value of the mean
+  #Get the value of the mean
 }
 
 
@@ -12,4 +16,5 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  #solve(x)
 }
